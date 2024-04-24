481b7b68429656dac202b649db8817a0a755b54a
Devin <dbelle722@gmail.com>
2024-04-24 18:39:25 -0400

    deleted comments

--- server/server.js
+++ server/server.js
@@ -18,35 +18,15 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  //introspection: true
-  //context: authMiddleware,
+  
 });
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-
-//   app.use(express.static(path.join(__dirname, '../client/dist')));
-
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-//   });
-// }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-// });
-
-//app.use(routes);
-//server.applyMiddleware({ app, path: '/graphql' });
-//Apollo server async function
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
 
 
-  //server.applyMiddleware({ app, path: '/graphql' });
+  
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
@@ -78,14 +58,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 };
 
-//later path for client side
-//console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
 
-
-// db.once('open', () => {
-//   //need graphql in the lidening port
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
-
-//Appolo server function call
 startApolloServer(typeDefs, resolvers);
