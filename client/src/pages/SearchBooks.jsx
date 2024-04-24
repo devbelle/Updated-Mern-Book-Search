18939b38c6fec6a474a97294ccbe7ed0412d2d55
Devin <dbelle722@gmail.com>
2024-04-24 17:40:13 -0400

    Final Edits

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,7 +9,7 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 //may need to import usemutation
@@ -72,18 +72,18 @@ const SearchBooks = () => {
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    // if (!token) {
+    //   return false;
+    // }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      await saveBook({variables:{bookData: bookToSave}});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  //introspection: true
   //context: authMiddleware,
 });
 
@@ -39,17 +40,20 @@ const server = new ApolloServer({
 // });
 
 //app.use(routes);
-
+//server.applyMiddleware({ app, path: '/graphql' });
 //Apollo server async function
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-  //server.applyMiddleware({ app });
+
+
+  //server.applyMiddleware({ app, path: '/graphql' });
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
 
   app.use('/graphql', expressMiddleware(server, {
+    //defiing where path will be
     context: authMiddleware
   }));
 
@@ -68,7 +72,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`)
     });
   });
 
